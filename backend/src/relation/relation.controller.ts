ba97bef208840cc3dcfd0d93a0fd1022672d1194
LaetitiaV <laetitia.voue@gmail.com>
2025-09-17 16:42:29 +0200

    DB&MCS-RelationOK

--- backend/src/relation/relation.controller.ts
+++ backend/src/relation/relation.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param, Body } from '@nestjs/common';
+import { RelationService } from './relation.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('relation')
-export class RelationController {}
+export class RelationController {
+    constructor(private readonly service: RelationService) {}
+
+    @Post()
+    envoyerRelation(@Body() body: { demandeurId: number; receveurId: number }) {
+        return this.service.envoyerRelation(body.demandeurId, body.receveurId);
+    }
+
+    @Get('utilisateur/:id')
+    lireRelationsUtilisateur(@Param('id') id: string) {
+        return this.service.lireRelationsUtilisateur(Number(id));
+    }
+
+    @Put(':id/statut')
+    mettreAJourStatut(
+        @Param('id') id: string,
+        @Body() body: { statut: Prisma.RelationUpdateInput['statut'] }
+    ) {
+        return this.service.mettreAJourStatut(Number(id), body.statut);
+    }
+
+    @Delete(':id')
+    supprimerRelation(@Param('id') id: string) {
+        return this.service.supprimerRelation(Number(id));
+    }
+}
--- backend/src/relation/relation.service.ts
+++ backend/src/relation/relation.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma, Relation } from '@prisma/client';
 
 @Injectable()
-export class RelationService {}
+export class RelationService {
+    constructor(private prisma: PrismaService) {}
+
+    async envoyerRelation(demandeurId: number, receveurId: number): Promise<Relation> {
+        return this.prisma.relation.create({
+            data: {
+                demandeurId,
+                receveurId,
+            },
+        });
+    }
+
+    async lireRelationsUtilisateur(utilisateurId: number) {
+        return this.prisma.relation.findMany({
+            where: {
+                statut: 'acceptee',
+                OR: [
+                    { demandeurId: utilisateurId },
+                    { receveurId: utilisateurId },
+                ],
+            },
+            include: { demandeur: true, receveur: true },
+        });
+    }
+
+    async mettreAJourStatut(id: number, statut: Prisma.RelationUpdateInput['statut']): Promise<Relation> {
+        return await this.prisma.relation.update({
+            where: { id },
+            data: { statut },
+            })
+    }
+
+    async supprimerRelation(id: number): Promise<Relation> {
+            return await this.prisma.relation.delete({ where: { id } });
+        }
+
+}
+
