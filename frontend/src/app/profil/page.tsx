cc31ca652e79791b5ac6b555a9d7b033e0167346
LaetitiaV <laetitia.voue@gmail.com>
2025-09-21 21:43:13 +0200

    Erreur FrontTest Profil/user.tsx

--- frontend/src/app/profil/page.tsx
+++ frontend/src/app/profil/page.tsx
@@ -46,7 +46,6 @@ export default function ProfilPage() {
                 }
             }
         };
-
             fetchUtilisateur();
         }
     }, [isLoaded, isSignedIn, user, BACKEND_URL]);
