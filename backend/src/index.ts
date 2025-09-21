3c7f1bc92ce9de9d7d8675a21e1025e2eb42324f
LaetitiaV <laetitia.voue@gmail.com>
2025-09-21 17:29:12 +0200

    Vercel+Express lasttest

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+const server = express();
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    await app.init();
+}
+bootstrap();
+
+export default server;
