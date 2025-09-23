daea1521f3be777dd6b887bd428e5d9a448c5003
LaetitiaV <laetitia.voue@gmail.com>
2025-09-23 20:21:24 +0200

    NewProjet FrontVercel changement index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,15 @@
+import serverless from 'serverless-http';
 import express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-const server = express();
+const app = express();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    await app.init();
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
+    await nestApp.init();
 }
 bootstrap();
 
-export default server;
+export const handler = serverless(app);
